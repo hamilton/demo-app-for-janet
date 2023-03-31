d31e5a8365466ef7a96762548fbd8a228b1ac48d
Hamilton Ulmer <hamilton.ulmer@gmail.com>
2023-03-31 15:04:31 -0700

    pushing a change

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,5 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
-</script>
+		</script>
 
 <svelte:head>
 	<title>Home</title>
@@ -11,21 +8,11 @@
 
 <section>
 	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
 
-		to your new<br />SvelteKit app
+
+		Welcome to JANET'S new sveltekit app!!!!!!!!!!!!!!!!!!!!!
 	</h1>
 
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
 </section>
 
 <style>
